b8751948b2a382233732fb9a93ea5796c5a286dd
grcodeman <grcodeman@gmail.com>
2025-03-30 12:54:26 -0400

    chat bubble :)

--- app/components/ChatBox.js
+++ app/components/ChatBox.js
@@ -26,7 +26,17 @@ export default function ChatBox() {
         }}
       >
         {messages.map((msg, index) => (
-          <div key={index} style={{ padding: '0.5rem', borderBottom: '1px solid #eee' }}>
+          <div
+            key={index}
+            style={{
+              maxWidth: '70%',
+              margin: '0.5rem 0',
+              padding: '0.75rem',
+              backgroundColor: '#DCF8C6',
+              borderRadius: '15px',
+              marginLeft: 'auto'
+            }}
+          >
             {msg}
           </div>
         ))}
@@ -37,7 +47,7 @@ export default function ChatBox() {
           placeholder="Type your message..."
           value={input}
           onChange={(e) => setInput(e.target.value)}
-          style={{ flexGrow: 1 }}
+          style={{ flexGrow: 1, marginRight: '0.5rem' }}
         />
         <button type="submit">Send</button>
       </form>
